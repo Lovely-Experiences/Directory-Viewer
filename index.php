947a5e1ecaf2167b784cee6196a23c1614023e0b
Jacob Humston's School Account <113031145+jacobhumston-school@users.noreply.github.com>
2023-03-28 16:49:27 +0000

    .

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Directory Viewer</title>
+</head>
+<body>
+    <?php
+
+    echo 'hi';
+
+    ?>
+</body>
+</html>
