5e9ac261c2d8459bab27aaec513df5b7501ce8ad
Jacob Humston's School Account <113031145+jacobhumston-school@users.noreply.github.com>
2023-09-20 15:34:21 +0000

    .

--- generate-test-files.cjs
+++ generate-test-files.cjs
@@ -3,9 +3,9 @@ const { v4: uuid } = require('uuid');
 
 if (!fs.existsSync('test/')) fs.mkdirSync('test/');
 
-for (i = 0; i < 10000; i++) {
+for (i = 0; i < 100000; i++) {
     fs.writeFileSync(`./test/${uuid()}.txt`, `${new Date().toString()}`);
-    console.log(`${i}/10000`);
+    console.log(`${i}/100000`);
 }
 
 console.log('done');
