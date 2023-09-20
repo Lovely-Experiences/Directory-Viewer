c949e65812f566b638faa181dc4d325130d8ec3a
Jacob Humston's School Account <113031145+jacobhumston-school@users.noreply.github.com>
2023-09-20 15:22:49 +0000

    fixed it (again)

--- generate-test-files.cjs
+++ generate-test-files.cjs
@@ -4,8 +4,8 @@ const { v4: uuid } = require('uuid');
 if (!fs.existsSync('test/')) fs.mkdirSync('test/');
 
 for (i = 0; i < 10000; i++) {
-    fs.writeFileSync(`${uuid()}.txt`, `${new Date().toString()}`);
-    console.log(`./test/${i}/10000`);
+    fs.writeFileSync(`./test/${uuid()}.txt`, `${new Date().toString()}`);
+    console.log(`${i}/10000`);
 }
 
 console.log('done');
